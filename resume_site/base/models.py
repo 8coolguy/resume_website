d4eadcf707e43b7821614477102b5d69879b623c
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-11-21 20:57:01 -0800

    models still messed up

--- resume_site/base/models.py
+++ resume_site/base/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 
 # Create your models here.
-class Skills(models.Model):
+#should override all of the __str__ functions
+class Skill(models.Model):
     skill_str=models.CharField(max_length=50)
-    date=models.DateTimeField('date_asked')
-
+    #date=models.DateTimeField('date_asked')
     def __str__(self):
         return self.skill_str
-class Projects(models.Model):
+class Project(models.Model):
     url_link=models.URLField(max_length=200)
     descrip_str =models.TextField(max_length=1000)
     title_str=models.CharField(max_length=100)
-    date_completed=models.DateTimeField('date_finished')
-    image_path=models.CharField(max_length=100)
-    skills_used=models.ForeignKey(Skills, on_delete=models.CASCADE)
+    date_completed=models.DateField('date_finished')
+    image_path=models.ImageField(upload_to='Images')
+    #skills_used=models.ForeignKey(Skill, on_delete=models.CASCADE)
     def __str__(self):
         return self.title_str
 
-class Jobs(models.Model):
+class Experience(models.Model):
     pass
     #weeeeeaaall you know'
     #need to have the skill database object connected here ya know
+class Classe(models.Model):
+    descrip_str=models.TextField(max_length=500)
+    title_str=models.CharField(max_length=100)
 class Eduaction(models.Model):
-    gpa_flt =models.
     school_str=models.CharField(max_length=200)
     major_str=models.CharField(max_length=150)
-    expec_grad=models.IntegerField()
-    cool_classes=models.ForeignKey(Class), on_delete)
-class Interests(models.Model):
-    image_path=models.CharField(max_length=100)
-    interest=models.CharField(max_length)
-    date=models.DateTimeField('date_asked')
+    expec_grad=models.DateField()
+
+class Interest(models.Model):
+    interest=models.CharField(max_length=100)
--- resume_site/base/views.py
+++ resume_site/base/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 
 # Create your views here
 def home(request):
-    skills
-    interests
-    projects
-    eduaction
-
+    #skills
+    #interests
+    #projects
+    #eduaction
     return render(request, 'base/index.html')
      
