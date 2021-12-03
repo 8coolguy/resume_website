4ddbb2bd550618a087853fc3cc09672a3ab811ec
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-03 12:36:36 -0800

    Made the file upload system work for thumbnails need to do the view for the homepage than html and css stuff to make the a page loook good

--- resume_site/base/views.py
+++ resume_site/base/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-
+from . models import Skill, Project, Eduaction, Interest
 # Create your views here
 def home(request):
-    #skills
-    #interests
-    #projects
-    #eduaction
+    resume ={}
+    #resume['skills']=Skill.objects
     return render(request, 'base/index.html')
      
--- resume_site/polls/views.py
+++ resume_site/polls/views.py
@@ -12,7 +12,6 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-
 def results(request, question_id):
     response = "You are looking at the respone of question %s"
     return HttpResponse(response %question_id)
