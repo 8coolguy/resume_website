cdbc63899f9c90ccafe30d54f1f600f078716e28
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-04 11:59:54 -0800

    made the base_template work. working on the navbar

--- resume_site/base/views.py
+++ resume_site/base/views.py
@@ -3,6 +3,11 @@ from . models import Skill, Project, Eduaction, Interest
 # Create your views here
 def home(request):
     resume ={}
-    #resume['skills']=Skill.objects
-    return render(request, 'base/index.html')
+    resume['skills']=Skill.objects.all()
+    resume['projects'] =Project.objects.all()
+    # for now im going to keep this unhiglighted
+    #resume['education']=Eduaction.objects.all()
+    resume['interests'] = Interest.objects.all()
+    #job_experience will come later 
+    return render(request, 'base/index.html',resume)
      
