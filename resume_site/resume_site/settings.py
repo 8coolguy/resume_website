7213bd277a152d5c4ae3cda781dce03cce2e95c5
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-19 23:32:38 -0800

    settings change

--- resume_site/resume_site/settings.py
+++ resume_site/resume_site/settings.py
@@ -28,13 +28,14 @@ ALLOWED_HOSTS = [
 # Application definition
 INSTALLED_APPS = [
     #'polls.apps.PollsConfig',
+    'base',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
+    
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
