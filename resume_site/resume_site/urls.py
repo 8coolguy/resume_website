51d51be3911aec38f80c6a317246776618df1c7c
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-20 14:36:03 -0800

    testing something

--- resume_site/resume_site/urls.py
+++ resume_site/resume_site/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     #path('home/', None)
-    path('', include('base.urls')),
+    #path('', include('base.urls')),
     #path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- resume_site/resume_site/wsgi.py
+++ resume_site/resume_site/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_site.resume_site.settings')
 print(os.getcwd())
+
+
+
 application = get_wsgi_application()
