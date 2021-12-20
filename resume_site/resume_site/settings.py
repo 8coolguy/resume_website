f1d4a017a74d6ba480600fb1d61dabde34ee467b
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-19 18:44:35 -0800

    thwee

--- resume_site/resume_site/settings.py
+++ resume_site/resume_site/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '3',
+    'base',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
