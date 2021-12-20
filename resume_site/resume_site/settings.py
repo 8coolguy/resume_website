492ef938102691c8396da62ff2402f7e9b96f809
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-19 18:38:17 -0800

    added django heroku

--- resume_site/resume_site/settings.py
+++ resume_site/resume_site/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
+    '3',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
