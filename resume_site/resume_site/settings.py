946063554885ccdac622956a69d7f3c026ce0f85
8coolguy <arnavchoudhury20@mittymonarch.com>
2021-12-21 11:48:38 -0800

    setting changes

--- resume_site/resume_site/settings.py
+++ resume_site/resume_site/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'base',
+    'resume_site.base',
     
 ]
 MIDDLEWARE = [
